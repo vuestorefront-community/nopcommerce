a4067f29b9740c5f3db7c586b88e620e04563e5d
Martin Gustafsson <gusten86@gmail.com>
2022-03-03 15:28:11 +0100

    perf(useFacet): reduce number of API calls

--- packages/composables/src/composables/useFacet/index.ts
+++ packages/composables/src/composables/useFacet/index.ts
@@ -3,11 +3,9 @@ import {
   FacetSearchResult,
   useFacetFactory
 } from '@vue-storefront/core';
-import type { CatalogProductsCommandDto, CategoryResponse, GetCategoryProductsResponse, ProductDetailsModelDto, ProductDetailsResponse, UrlRecordDto } from '@vue-storefront/nopcommerce-api/gen';
+import type { CatalogProductsCommandDto, CategoryResponse, UrlRecordDto } from '@vue-storefront/nopcommerce-api/gen';
 
 const factoryParams = {
-
-  /* TODO: Many API called in search function. Need to find solution to reduce API call */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   search: async (context: Context, params?: FacetSearchResult<CatalogProductsCommandDto>) => {
     const criterias: CatalogProductsCommandDto = {
@@ -20,22 +18,13 @@ const factoryParams = {
 
     const urlRecordDto : UrlRecordDto = await context.$nopcommerce.api.urlRecordGetBySlugGet({slug: params.input.term});
 
-    const rootCategoryResponse : CategoryResponse = await context.$nopcommerce.api.catalogGetCategoryCategoryIdPost({categoryId: urlRecordDto.entity_id});
-
-    const rootCategoryProductsResponse:GetCategoryProductsResponse = await context.$nopcommerce.api.catalogGetCategoryProductsCategoryIdPost({categoryId: urlRecordDto.entity_id, catalogProductsCommandDto: criterias});
-
-    const products = new Array<ProductDetailsModelDto>();
-
-    for (const productOverview of rootCategoryProductsResponse.catalog_products_model.products) {
-      const response: ProductDetailsResponse = await context.$nopcommerce.api.productGetProductDetailsProductIdGet({productId: productOverview.id});
-      products.push(response.product_details_model);
-    }
+    const rootCategoryResponse : CategoryResponse = await context.$nopcommerce.api.catalogGetCategoryCategoryIdPost({categoryId: urlRecordDto.entity_id, catalogProductsCommandDto: criterias});
 
     return {
       catalog_model: rootCategoryResponse.category_model_dto,
-      catalog_products_model: rootCategoryProductsResponse.catalog_products_model,
-      products: products,
-      template_view_path: rootCategoryProductsResponse.template_view_path
+      catalog_products_model: rootCategoryResponse.category_model_dto.catalog_products_model,
+      products: rootCategoryResponse.category_model_dto.catalog_products_model.products,
+      template_view_path: rootCategoryResponse.template_view_path
     };
   }
 };
